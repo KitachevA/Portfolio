102644889d7b34556df20e767f03e0d834265de8
Artem <periphery290@gmail.com>
2023-02-15 13:32:33 +0200

    section one+two

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeMenu = document.querySelector('.menu__close');
+
+
+
+
+      hamburger.addEventListener('click', ()=>{
+        menu.classList.add('active');
+      });
+
+      closeMenu.addEventListener('click', ()=>{
+            menu.classList.remove('active');
+      });
